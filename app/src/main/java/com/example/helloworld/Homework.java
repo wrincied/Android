b9a40568412fd7cadbf3352d03f84945e5e23517
Senya Mileuski <republicofsport2020@gmail.com>
2022-11-12 15:31:00 +0300

    Homework branch

---
+++ app/src/main/java/com/example/helloworld/Homework.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/Math.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class Math {
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        printInConsole(a);
+    }
+
+    static void printInConsole(int a) {
+//        switch (a){
+//            case (1):
+//                System.out.println("Один");
+//            break;
+//            case (2):
+//                System.out.println("Два");
+//            break;
+//            default:
+//                System.out.println("Другое число, ни 1, ни 2,");
+//                System.out.println("Ваше число:" + a);
+//        }
+//        for (int i = 0; i<10; i++){
+//            System.out.println(i);
+//        }
+
+    }
+}
