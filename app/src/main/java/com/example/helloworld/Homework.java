a99ecec973181da70b3ff1d6f8eed339499a8c14
Senya Mileuski <republicofsport2020@gmail.com>
2022-11-12 15:45:19 +0300

    Homework branch

--- app/src/main/java/com/example/helloworld/Homework.java
+++ app/src/main/java/com/example/helloworld/Homework.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import java.util.Scanner;
 
 public class Homework {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
     }
 }
