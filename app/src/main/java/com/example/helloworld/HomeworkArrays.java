5ab51e82f6e5ee995a106e5fd4abc4610e2292eb
Senya Mileuski <republicofsport2020@gmail.com>
2022-11-15 16:50:53 +0300

    homework task1

--- app/src/main/java/com/example/helloworld/HomeworkArrays.java
+++ app/src/main/java/com/example/helloworld/HomeworkArrays.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class HomeworkArrays {
     public static void main(String[] args) {
+        task1();
     }
 
+    static void task1() {
+        String[] names = {"Roma", "Jan", "John", "Liza"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+    }
 }
+
