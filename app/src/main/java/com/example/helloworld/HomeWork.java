dacce5de8abf68a2eab5e75dcd7977609a4c4ddb
Senya Mileuski <republicofsport2020@gmail.com>
2022-11-20 11:46:11 +0300

    homework branch

---
+++ app/src/main/java/com/example/helloworld/HomeWork.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+
+        task1();
+    }
+
+    static void task1() {
+
+    }
+}
+
+
